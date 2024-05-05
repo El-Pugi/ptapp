c0ccff928b4b31a292c0368c1757b741c4145d71
El-Pugi <Simonen.pauli@hotmail.com>
2024-05-05 18:52:13 +0300

    kalenterin poisto, sekottaa gettaining

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Inject, ScheduleComponent, Day, Week, Month, Agenda} from '@syncfusion/ej2-react-schedule';
 
 export default function Calendar(){
   return(
-    <ScheduleComponent>
-      <Inject services={[Day, Week, Month, Agenda]} />
-    </ScheduleComponent>
+    <>
+    </>
   )
 
 }
